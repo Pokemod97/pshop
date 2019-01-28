912604ed6df5677cd7c2a9fb49c26dcd3a23ac3e
Caden Kline <cadenkline9@gmail.com>
2019-01-28 08:02:15 -0500

    step 1

--- shop.py
+++ shop.py
@@ -1,7 +1,10 @@
 import argparse
+import PIL
 
 def main():
-    print "hi"
-
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mode", help="choose photo mode")
+    pictures = parser.add_mutually_exclusive_group()
+    
 if __name__ == '__main__':
     main()
